325ef1744045e1f41fc1bf97c70812c032ce1fc2
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-06 20:34:15 -0300

    Feat: Data
    Add admin data definition

--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -1,11 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using minimal_api.Domain.Entities;
 
 namespace minimal_api.Infrastructure.Data
 {
     public class DataContext : DbContext
     {
-       public DataContext(DbContextOptions<DataContext> options) : base(options)
-       {} 
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
 
+        public DbSet<Admin> Admins { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
