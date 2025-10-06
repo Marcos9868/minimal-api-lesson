8b2d54d593885e7d6a5a59c5f5aeccd571bcd08c
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-06 20:33:58 -0300

    Feat: Entity
    Add admin entity on domain layer

---
+++ Domain/Entities/Admin.cs
@@ -0,0 +1,10 @@
+namespace minimal_api.Domain.Entities
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
