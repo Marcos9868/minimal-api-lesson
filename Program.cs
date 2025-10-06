aa937209342ef6d2c4335893deaaad5ca82d333a
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-06 20:09:40 -0300

    Feat: initial routes
    Add post route on program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using MinimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapPost("/login", (LoginDTO loginDto) => 
+{
+  if (loginDto.Email == "admin@teste.com" && loginDto.Password == "123456")
+  {
+    return Results.Ok("Login Done");
+  }
+  else
+  {
+    return Results.Unauthorized();
+  }
+});
 
 app.Run();
