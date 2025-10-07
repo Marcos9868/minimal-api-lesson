62e571b960dbf8c007610fc9c85f7bb70365994b
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-07 19:57:52 -0300

    Feat: migrations
    Add admin column

---
+++ Migrations/20251007224432_ProfileColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minimal_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfileColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Profile",
+                table: "Admins",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Profile",
+                table: "Admins");
+        }
+    }
+}
