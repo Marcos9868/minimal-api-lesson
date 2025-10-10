69495d14fd66647f62f91502908cc74b0b561b0d
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-10 20:38:40 -0300

    Fix: service
    come back on indent code

--- Application/Services/AdministratorService.cs
+++ Application/Services/AdministratorService.cs
@@ -7,12 +7,12 @@ namespace minimal_api.Application.Services
 {
   public class AdministratorService : IAdministratorService
   {
-        private readonly DataContext _context;
-        public AdministratorService(DataContext context) => _context = context;
-        public List<Admin> Login(LoginDTO login)
-        {
-            var adminQuantity = _context.Admins.Where(a => a.Email == login.Email);
-            return adminQuantity.ToList();
-        }
+    private readonly DataContext _context;
+    public AdministratorService(DataContext context) => _context = context;
+    public List<Admin> Login(LoginDTO login)
+    {
+      var adminQuantity = _context.Admins.Where(a => a.Email == login.Email);
+      return adminQuantity.ToList();
+    }
   }
 }
