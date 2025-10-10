f19e3429be3c8bb8758bad5fafcb8d38f846a31d
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-10 20:40:17 -0300

    Feat: Migrations
    Add migration that add vehicle table on database

--- Migrations/20251010231231_CreateVehicleTable.Designer.cs
+++ Migrations/20251010231231_CreateVehicleTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using minimal_api.Infrastructure.Data;
 
@@ -10,9 +11,11 @@ using minimal_api.Infrastructure.Data;
 namespace minimal_api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20251010231231_CreateVehicleTable")]
+    partial class CreateVehicleTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
---
+++ Migrations/20251010231231_CreateVehicleTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minimal_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateVehicleTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    VehicleMark = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    ReleaseYear = table.Column<int>(type: "INTEGER", maxLength: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
