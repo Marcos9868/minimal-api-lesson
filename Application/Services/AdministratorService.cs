1c5d3cf75dbfd2d24728b4596648a5fe4ed27fa2
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-14 18:59:04 -0300

    Feat: service
    Add more functions for admin service

--- Application/Services/AdministratorService.cs
+++ Application/Services/AdministratorService.cs
@@ -9,10 +9,28 @@ namespace minimal_api.Application.Services
   {
     private readonly DataContext _context;
     public AdministratorService(DataContext context) => _context = context;
-    public List<Admin> Login(LoginDTO login)
+    public Admin? Login(LoginDTO login)
     {
-      var adminQuantity = _context.Admins.Where(a => a.Email == login.Email);
-      return adminQuantity.ToList();
+      var adminQuantity = _context.Admins.FirstOrDefault(a => a.Email == login.Email);
+      return adminQuantity;
+    }
+    public Admin AddAdmin(Admin admin)
+    {
+      _context.Admins.Add(admin);
+      _context.SaveChanges();
+      return admin;
+    }
+    public List<Admin> All(int? page)
+    {
+      var query = _context.Admins.AsQueryable();
+      int itemsPerPage = 10;
+      if (page != null)
+        query = query.Skip(((int)page - 1) * itemsPerPage).Take(itemsPerPage);
+      return query.ToList();
+    }
+    public Admin? SearchById(int id)
+    {
+      return _context.Admins.FirstOrDefault(a => a.Id == id);
     }
   }
 }
