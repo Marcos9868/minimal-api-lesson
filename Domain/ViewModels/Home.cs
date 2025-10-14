ed930335bb7e49981057d8fe7a9bedf730169439
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-10-14 18:38:57 -0300

    Feat: ViewModel
    Add Home page for minimal api

---
+++ Domain/ViewModels/Home.cs
@@ -0,0 +1,8 @@
+namespace minimal_api.Domain.ViewModels
+{
+    public struct Home
+    {
+        public string Message { get => "Welcome to Vehicle API -> Minimal API Type"; }
+        public string Doc { get => "/swagger"; }        
+    }
+}
